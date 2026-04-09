6d73b59d39ca21948f337f9ecfbf5bd466565a4f
Ben Davis <bengineerdavis@gmail.com>
2026-04-09 16:08:30 -0700

    Refactors apps' topics for the new migrated schema version

---
+++ dotfiles/apps/trash-cli/files/zsh/executable_path.zsh
@@ -0,0 +1,2 @@
+# specifically for m3/m4+ macs?
+export PATH="/opt/homebrew/opt/trash-cli/bin:$PATH"
---
+++ dotfiles/apps/visual-studio-code/files/zsh/executable_env.zsh
@@ -0,0 +1,2 @@
+# from google search AI chat: https://share.google/aimode/qIiXZgqb72IilEOXd
+export EDITOR=~/bin/vscode-wait.sh
