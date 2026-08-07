57dd1dd4e77be949c66f170ed137f4db179bb67e
Ben Davis <bengineerdavis@gmail.com>
2026-08-06 21:08:54 -0700

    resolves conflicts from dotfiles remote

--- dotfiles/zsh/executable_aliases.zsh
+++ dotfiles/zsh/executable_aliases.zsh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
 alias cz='smart_wrap chezmoi help'
 alias openmail='mailbox "attachments/**" && pii-redactor -i mail.md -o mail-pii-removed.md'
 alias deliver='clipped p > attachments/emails.txt'
+alias kobo='export WINEPREFIX="$HOME/.wine"; wine "$WINEPREFIX/drive_c/Program Files (x86)/Kobo/Kobo Desktop/KoboDesktop.exe" &>/dev/null & disown'
+
+# source: "Could not get lock /var/lib/apt/lists/lock" 
+# https://askubuntu.com/a/1545774
+alias unlock-apt='sudo service packagekit restart'
